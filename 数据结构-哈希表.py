ce0590fce153358326fe9aa51e4f5b7cd5f1916f
ZJD <2104376503@qq.com>
2025-03-25 23:43:30 +0800

    哈希表开始

---
+++ 数据结构-哈希表.py
@@ -0,0 +1,49 @@
+class LinkList:
+    class Node:
+        def __init__(self, item=None):
+            self.item = item
+            self.next = None
+
+    class LinkListIterator:
+        def __init__(self, node):
+            self.node = node
+        def __next__(self):
+            if self.node:
+                cur_node = self.node
+                self.node = cur_node.next
+                return cur_node.item
+            else:
+                raise StopIteration
+        def __iter__(self):
+            return self
+
+    def __init__(self, iterable=None):
+        self.head = None
+        self.tail = None
+        if iterable:
+            self.extend(iterable)
+    def append(self, obj):
+        s = LinkList.Node(obj)
+        if not self.head:
+            self.head = s
+            self.tail = s
+        else:
+            self.tail.next = s
+            self.tail = s
+
+    def extend(self, iterable):
+        for obj in iterable:
+            self.append(obj)
+
+    def find(self, obj):
+        for n in self:
+            if n == obj:
+                return True
+        else:
+            return False
+
+    def __iter__(self):
+        return self.LinkListIterator(self.head)
+
+    def __repr__(self):
+        return "<<"+"，".join(map(str, self))+">>"
