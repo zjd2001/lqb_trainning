5130fb61a15fb58cd9d229a76e30a46ad1482904
ZJD <2104376503@qq.com>
2025-03-28 12:00:39 +0800

    二叉搜索树

--- 数据结构-树.py
+++ 数据结构-树.py
@@ -123,14 +123,19 @@ def leval_order(root):
         if node.rchild:
             queue.append(node.rchild)
     # 循环直到队列为空，表示所有节点都已访问完毕
-leval_order(root)
+
+#leval_order(root)
 
 
-#二叉搜索树
+#二叉搜索树：对任意根或子根，左子树都比根小，右子树都比根大
 class BST:
-    def __init__(self):
+    def __init__(self, li=None):
         self.root = None
+        if li: # 输入列表存在
+            for val in li:
+                self.insert_no_rec(val) # 循环插入
 
+    # 插入，新节点总是作为叶子节点插入
     # 递归实现插入
     def insert(self, node, val): # node：要插入的节点,  val：需要插入的值
         if not node: # 节点为空
@@ -145,17 +150,17 @@ class BST:
 
     # 不使用递归实现插入
     def insert_no_rec(self, val):
-        p = self.root
-        if not p: #空树
+        p = self.root # 指向当前节点的指针，从根节点开始，逐步遍历树以找到合适的位置插入新值
+        if not p: # 空树
             self.root = BiTreeNode(val)
             return
         while True:
-            if val < p.data:
+            if val < p.data: # 要插入的值 val 小于当前节点的值 p.data，则应该插入到左子树
                 if p.lchild:
-                    p = p.lchild
-                else: # 左孩子不存在
+                    p = p.lchild # 如果左子节点存在，则移动指针 p 到左子节点继续查找
+                else:
                     p.lchild = BiTreeNode(val)
-                    p.lchild.parent = p
+                    p.lchild.parent = p # 如果左子节点不存在，则创建一个新节点，赋值为 val，并将其设置为当前节点的左子节点，同时设置其父节点为 p。
             elif val > p.data:
                 if p.rchild:
                     p = p.rchild
@@ -164,6 +169,50 @@ class BST:
                     p.rchild.parent = p
                     return
             else:
-                return
+                return # 要插入的值 val 等于当前节点的值 p.data，则直接退出函数。这表明不允许插入重复值
 
+    # 查询
+    # 使用递归
+    def query(self, node, val): # 需要查询的值val，node初始为根节点用于递归，后为正在比较的节点
+        if not node:
+            return None
+        if node.data < val:
+            return self.query(node.rchild, val)
+        elif node.dta > val:
+            return self.query(node.lchild, val)
+        else:
+            return node # val所在节点
 
+    # 不使用递归
+    def query_no_rec(self, val):
+
+    # 前序遍历
+    def pre_order(self, root):
+        if root:
+            print(root.data, end=',')
+            self.pre_order(root.lchild)
+            self.pre_order(root.rchild)
+
+    # 中序遍历
+    def in_order(self, root):
+        if root:
+            self.in_order(root.lchild)
+            print(root.data, end=',')
+            self.in_order(root.rchild)
+
+    # 后序遍历
+    def post_order(self, root):
+        if root:
+            self.post_order(root.lchild)
+            self.post_order(root.rchild)
+            print(root.data, end=',')
+
+"""
+插入
+tree = BST([4,6,7,9,2,1,3,5,8])
+tree.post_order(tree.root)
+print("")
+tree.in_order(tree.root)
+print("")
+tree.post_order(tree.root)
+"""
