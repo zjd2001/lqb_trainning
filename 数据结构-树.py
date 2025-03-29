5226978463fdcf97cdfc29ecf316aab62f3561fc
ZJD <2104376503@qq.com>
2025-03-29 23:29:38 +0800

    AVL树

--- 数据结构-树.py
+++ 数据结构-树.py
@@ -162,6 +162,7 @@ class BST:
                 else:
                     p.lchild = BiTreeNode(val)
                     p.lchild.parent = p # 如果左子节点不存在，则创建一个新节点，赋值为 val，并将其设置为当前节点的左子节点，同时设置其父节点为 p。
+                    return
             elif val > p.data:
                 if p.rchild:
                     p = p.rchild
@@ -320,3 +321,193 @@ tree.in_order(tree.root)
 
 # AVL树：自平衡的二叉搜索树（查找、插入效率高）
 # 根的左右子树的高度之差绝对值不超过1，且跟的左右子树都是平衡二叉树
+
+class AVLNode(BiTreeNode):
+    def __init__(self, data):
+        BiTreeNode.__init__(self, data)
+        self.bf = 0 # 平衡因子：右树高度减去左树，绝对值不能大于1
+
+class AVLTree(BST):
+    def __init__(self, li=None):
+        BST.__init__(self, li)
+
+    #插入导致不平衡后，需要通过旋转重新平衡
+    #左旋：不平衡是由于对根的右孩子的右子树的插入导致的
+    def rotate_left(self, p, c):
+        s2 = c.lchild
+        p.rchild = s2
+        if s2:
+            s2.parent = p
+
+        c.lchild = p
+        p.parent = c
+
+        p.bf = 0 # 更新bf
+        c.bf = 0
+
+        return c # 返回旋转后子树根c
+
+    #右旋：不平衡是由于对根的左孩子的左子树的插入导致的
+    def rotate_right(self, p, c):
+        s2 = c.rchild
+        p.lchild = s2
+        if s2:
+            s2.parent = p
+
+        c.rchild = p
+        p.parent = c
+
+        p.bf = 0
+        c.bf = 0
+
+        return c
+
+    # 右旋-左旋：不平衡是由于对根的右孩子的左子树的插入导致的
+    def roate_right_left(self, p, c):
+        g = c.lchild
+
+        s3 = g.rchild
+        c.lchild = s3
+        if s3:
+            s3.parent = c
+        g.rchild = c
+        c.parent = g
+
+        s2 = g.lchild
+        p.rchild = s2
+        if s2:
+            s2.parent = p
+        g.lchild = p
+        p.parent = g
+
+        if g.bf > 0: #插入到了s3上
+            p.bf = -1
+            c.bf = 0
+        elif g.bf < 0: #插入到了s2上
+            p.bf = 0
+            c.bf = 1
+        else: # s1-4都是空，插入的实际上是g
+            p.bf = 0
+            c.bf = 0
+
+        return g
+
+    # 左旋-右旋：不平衡是由于对根的左孩子的右子树的插入导致的
+    def rotate_left_right(self, p, c):
+        g = c.rchild
+
+        s2 = g.lchild
+        c.rchild = s2
+        if s2:
+            s2.parent = c
+        g.lchild = c
+        c.parent = g
+
+        s3 = g.rchild
+        p.lchild = s3
+        if s3:
+            s3.parent = p
+        g.lchild = p
+        p.parent = g
+
+        if g.bf < 0:
+            p.bf = 1
+            c.bf = 0
+        elif g.bf > 0:
+            p.bf = 0
+            c.bf = -1
+        else:
+            p.bf = 0
+            c.bf = 0
+
+        return g
+
+
+    def insert_no_rec(self, val):
+
+        # 1.插入，和BST的插入基本一样
+        p = self.root
+        if not p:  # 空树
+            self.root = AVLNode(val)
+            return
+        while True:
+            if val < p.data:
+                if p.lchild:
+                    p = p.lchild
+                else:
+                    p.lchild = AVLNode(val)
+                    p.lchild.parent = p
+
+                    node = p.lchild # 不同：添加node存储插入的节点位置
+                    break
+
+            elif val > p.data:
+                if p.rchild:
+                    p = p.rchild
+                else:
+                    p.rchild = AVLNode(val)
+                    p.rchild.parent = p
+
+                    node = p.rchild
+                    break
+
+            else:
+                return
+
+        # 2.更新平衡因子
+        while node.parent: # node.parent不空
+            if node.parent.lchild == node: # 传递是从左子树来，左子树更沉了
+                # 插入后node.parent的bf应减1
+                if node.parent.bf < 0: # 原来node.parent的bf是-1，则插入后变成了-2。需要做旋转
+                    # 做旋转
+                    # 看node哪边沉
+                    g = node.parent.parent # 为了连接旋转之后的子树               #node不是插入的叶子吗，为什么成了中间节点:插入阶段,node 指向插入的叶子节点.更新平衡因子阶段,node 从叶子节点开始逐层向上移动，指向每一层正在处理的节点
+                    x = node.parent # 旋转前子树的根
+                    if node.bf > 0:                                            #node.bf不会等于0吗：意味着当前节点的左右子树高度相等，这种情况不会直接导致父节点的 bf 变为 -2 或 2
+                        n = self.rotate_left_right(node.parent, node)
+                    else:
+                        n = self.rotate_right(node.parent, node)
+                    # 之后还需要把n和g连起来
+                elif node.parent.bf > 0: # 原来node.parent的bf是1，则插入后变成0.   #为什么是原来的，此时不是已经插入了变成0了吗:因为插入后还没更新过bf，此时bf还是之前的1
+                    node.parent.bf = 0
+                    break
+                else: # 原来的node.parent.bf是0，插入后变成-1
+                    node.parent.bf = -1
+                    node = node.parent # 再向上走一层，看还有没有出现需要旋转的
+                    continue
+
+            else: # 传递是从右子树来的，右子树更沉了
+                # 插入后node.parent的bf应加1
+                if node.parent.bf > 0: # 原来node.parent的bf是1，则插入后变成了2。需要做旋转
+                    # 做旋转
+                    # 看node哪边沉
+                    g = node.parent.parent
+                    x = node.parent
+                    if node.bf < 0: #左边沉
+                        n = self.roate_right_left(node.parent, node)
+                    else: # 右边沉
+                        n = self.rotate_left(node.parent, node)
+                elif node.parent.bf < 0: # 原来node.parent的bf是-1，则插入后变成了0
+                    node.parent.bf = 0
+                    break
+                else: # 原来的node.parent.bf是0，插入后变成1
+                    node.parent.bf = 1
+                    node = node.parent
+                    continue
+
+            # 连接旋转后的子树
+            n.parent = g # n是子树新的根，x是子树旋转前的根
+            if g: # g不空
+                if x == g.lchild: # 子树原本是上一节点的左子树
+                    g.lchild = n
+                else:
+                    g.rchild = n
+                break
+            else:
+                self.root = n
+                break
+
+tree = AVLTree([9,8,7,6,5,4,3,2,1])
+tree.pre_order(tree.root)
+print("")
+tree.in_order(tree.root)
