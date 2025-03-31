516072ee23cb10ecf5be6fbb945d4e5560a60129
ZJD <2104376503@qq.com>
2025-03-31 12:09:38 +0800

    贪心算法-分数背包

--- 清华python算法+数据结构基础/贪心算法.py
+++ 清华python算法+数据结构基础/贪心算法.py
@@ -11,19 +11,22 @@ def change(t, n):
 #print(change(t, 376))
 
 # 分数背包问题
-goods = [(60, 10), (100, 20), (120, 30)] # 每个商品的价格和重量
+goods = [(60, 10), (120, 30), (100, 20)] # 每个商品的价格和重量
 goods.sort(key=lambda x: x[0]/x[1], reverse=True) #按单位价值降序排序
-
+print(goods)
 def fractional_backpack(goods, w): # w是背包容量
     m = [0 for _ in range(len(goods))] # 存每种商品拿多少走
+    total_v = 0 # 装走的总价值
     for i, (prize, weight) in enumerate(goods):
         if w >= weight:
             m[i] = 1
+            total_v += prize
             w -= weight
         else:
             m[i] = w / weight # 这种商品只能拿一部分，背包就被装满了
+            total_v += m[i] * prize
             w = 0
             break
-    return m
+    return total_v, m
 
 print(fractional_backpack(goods, 50))
